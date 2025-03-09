91b09128dc0306ad6dacb1f419926a826756a063
WezeDevelop <103280050+WezeDevelop@users.noreply.github.com>
2025-03-09 06:39:30 +0200

    Add files via upload

---
+++ OOP-5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть рядок: ");
+        string input = Console.ReadLine();
+
+        // а) Видалення всіх цифр
+        string withoutDigits = new string(input.Where(c => !char.IsDigit(c)).ToArray());
+
+        // б) Подвоєння одинарних пробілів
+        string doubledSpaces = withoutDigits.Replace(" ", "  ");
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(doubledSpaces);
+    }
+}
